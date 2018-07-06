9124fd3e99e1f868ce2c808f7ac648a7b3d4449d
Joanna Gora <joanna.gora@protonmail.com>
2018-07-06 16:11:13 +0100

    passed play test

--- song.rb
+++ song.rb
@@ -7,5 +7,7 @@ class Song
     @artist = artist
   end
 
-
+  def play
+    return "Tudumdum Tudumdum #{lyrics}"
+  end
 end
--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -9,7 +9,7 @@ class TestSong < MiniTest::Test
     I ride through the city's backsides
     I see the stars come out of the sky
     Yeah, they're bright in a hollow sky
-    You know it looks so good tonight"
+    You know it looks so good tonight..."
     @song1 = Song.new("Passenger", lyrics, "Iggy Pop")
   end
 
@@ -25,8 +25,25 @@ class TestSong < MiniTest::Test
     I ride through the city's backsides
     I see the stars come out of the sky
     Yeah, they're bright in a hollow sky
-    You know it looks so good tonight"
+    You know it looks so good tonight..."
     actual = @song1.lyrics
+    assert_equal(expected, actual)
+  end
+
+  def test_artist
+    expected = "Iggy Pop"
+    actual = @song1.artist
+    assert_equal(expected, actual)
+  end
+
+  def test_play
+    expected = "Tudumdum Tudumdum I am the passenger
+    And I ride and I ride
+    I ride through the city's backsides
+    I see the stars come out of the sky
+    Yeah, they're bright in a hollow sky
+    You know it looks so good tonight..."
+    actual = @song1.play
     assert_equal(expected, actual)
   end
 end
