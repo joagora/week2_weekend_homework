d5391b3555a841b36cdb851bb439c0e1e04fb0b9
Joanna Gora <joanna.gora@protonmail.com>
2018-07-06 16:06:48 +0100

    passed lyrics test

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -18,4 +18,15 @@ class TestSong < MiniTest::Test
     actual = @song1.title
     assert_equal(expected, actual)
   end
+
+  def test_lyrics
+    expected = "I am the passenger
+    And I ride and I ride
+    I ride through the city's backsides
+    I see the stars come out of the sky
+    Yeah, they're bright in a hollow sky
+    You know it looks so good tonight"
+    actual = @song1.lyrics
+    assert_equal(expected, actual)
+  end
 end
